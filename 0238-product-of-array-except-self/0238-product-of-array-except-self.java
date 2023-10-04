334fc755f0dde257d0d8cb51195fba1b6158d552
Vinay C N <70995499+Vinay-CN@users.noreply.github.com>
2023-10-04 21:55:25 +0530

    Time: 2 ms (66.7%), Space: 52.6 MB (58.08%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,5 +1,37 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
+             //  O(N)
+        int n = nums.length;
+        int countZeroes = 0;
+        int indexZero = -1;
+        int productWithoutZero = 1;
+        
+        for(int i = 0 ; i < n ; i++) {
+            if(nums[i] == 0) {
+                countZeroes++;
+                indexZero = i;
+            }
+            else {
+                productWithoutZero *= nums[i];
+            }
+        }
+        
+        int [] output = new int [n];
+        if(countZeroes == 0) {
+            for(int i = 0 ; i < n ; i++) {
+                output[i] = productWithoutZero / nums[i];
+            }
+        }
+        else if(countZeroes == 1) {
+            output[indexZero] = productWithoutZero;
+        }
+        return output;
+    }
+}
+
+
+/*class Solution {
+    public int[] productExceptSelf(int[] nums) {
         int n = nums.length ;
       int[] pref = new int[n];
       pref[0] = nums[0];
@@ -19,4 +51,4 @@ class Solution {
         }
         return ans;
     }
-}
+}*/
