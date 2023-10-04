6dfb4d9e1ac329d6644e5090385cd9cf41afb773
Vinay C N <70995499+Vinay-CN@users.noreply.github.com>
2023-10-04 17:32:37 +0530

    Time: 3 ms (13.93%), Space: 53.3 MB (31.75%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -6,16 +6,16 @@ class Solution {
       int[] suf = new int[n];
       suf[n-1] = nums[n-1];
         for(int i = 1 ; i < n ; i++){
-            pref[i] = pref[i-1]*nums[i];
-            suf[n-i-1] = nums[n-i-1]*suf[n-i];
+            pref[i] = pref[i-1]*nums[i]; //get suffix
+            suf[n-i-1] = nums[n-i-1]*suf[n-i]; //get prefix
         }
 
         int[] ans = new int[n];
         for(int i = 0 ; i < n ; i++){
-           if(i==0)  ans[i] = suf[i+1] ; 
-           else if(i==n-1) ans[i] = pref[i-1]; 
+           if(i==0)  ans[i] = suf[i+1] ;  //for first element suffix till that element will be product
+           else if(i==n-1) ans[i] = pref[i-1]; //for last element prefix till that ellement will be product
            else 
-            ans[i] = pref[i-1]*suf[i+1];
+            ans[i] = pref[i-1]*suf[i+1]; //product till me * product after me;
         }
         return ans;
     }
