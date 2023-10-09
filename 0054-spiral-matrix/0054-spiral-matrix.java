f4ce34b46e5afc2eab790df51224c4777a782278
Vinay C N <70995499+Vinay-CN@users.noreply.github.com>
2023-10-09 15:52:23 +0530

    Time: 0 ms (100%), Space: 40.8 MB (18.91%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -52,47 +52,58 @@ class Solution {
 }
 
 
-// class Solution {
-//     public List<Integer> spiralOrder(int[][] matrix) {
-//         List<Integer> result = new ArrayList<>();
-//         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-//             return result;
-//         }
+/*class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
 
-//         int top = 0, bottom = matrix.length - 1, left = 0, right = matrix[0].length - 1;
+        int top = 0;
+        int bottom = matrix.length-1;
+        int left = 0;
+        int right = matrix[0].length-1;
 
-//         while (top <= bottom && left <= right) {
-//             // Traverse from left to right
-//             for (int i = left; i <= right; i++) {
-//                 result.add(matrix[top][i]);
-//             }
-//             top++;
+        List<Integer> res = new ArrayList<Integer>();
 
-//             // Traverse from top to bottom
-//             for (int i = top; i <= bottom; i++) {
-//                 result.add(matrix[i][right]);
-//             }
-//             right--;
+        if(bottom==0) {  //if only one row just print the row and return
+            for(int i=0; i<=right; i++) {
+                res.add(matrix[0][i]);
+            }
+            return res;
+        }
 
-//             // Ensure there are more rows to traverse
-//             if (top <= bottom) {
-//                 // Traverse from right to left
-//                 for (int i = right; i >= left; i--) {
-//                     result.add(matrix[bottom][i]);
-//                 }
-//                 bottom--;
-//             }
+        if(right==0) {
+            for(int i=0; i<=bottom; i++) {
+                res.add(matrix[i][0]);
+            }
+            return res;
+        }
 
-//             // Ensure there are more columns to traverse
-//             if (left <= right) {
-//                 // Traverse from bottom to top
-//                 for (int i = bottom; i >= top; i--) {
-//                     result.add(matrix[i][left]);
-//                 }
-//                 left++;
-//             }
-//         }
+        while(top<=bottom && left<=right) {
+            
+            for(int i=left; i<=right; i++) {
+                res.add(matrix[top][i]);
+            }
+            top++;
 
-//         return result;
-//     }
-// }
+            for(int i=top; i<=bottom; i++) {
+                res.add(matrix[i][right]);
+            }
+            right--;
+
+            if(top<=bottom) {
+                for(int i=right; i>=left; i--) {
+                    res.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            if(left<=right) {
+                for(int i=bottom; i>=top; i--) {
+                    res.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        return res;
+        
+    }
+}*/
