dbb9342fa92bb07f2160d941d0924bd5679ffb89
Vinay C N <70995499+Vinay-CN@users.noreply.github.com>
2023-10-10 18:55:38 +0530

    Time: 0 ms (100%), Space: 40.4 MB (96.83%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -6,8 +6,8 @@ class Solution {
     }
     public void sortColors(int[] nums) {
         int k = nums.length -1 ;
-        while(nums[k]==2 && k>0) k--;
-        int i = 0 , j = 0 ;
+        while(nums[k]==2 && k>0) k--; //k+1 to end must be 2's region so we will decrease k till we find non - 2 element
+        int i = 0 , j = 0 ; //i -1 to 0 is 0's region i to j is 1's region ;
         while(j<=k){
             if(nums[j]==0){
                 swap(nums,i,j);
