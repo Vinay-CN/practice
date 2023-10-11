c2d57a764cc40b27455cb3174838aefc0d3e85b3
Vinay C N <70995499+Vinay-CN@users.noreply.github.com>
2023-10-11 20:07:17 +0530

    Time: 1 ms (50.71%), Space: 55.9 MB (37.88%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -9,7 +9,7 @@ class Solution {
         }
     }
     public void rotate(int[] nums, int k) {
-        k = k%nums.length ;
+        k = k%nums.length ; //if k == nums.length then rotated array will be same so we need to rotate it k%n times;
         reverse(nums,0,nums.length-1);
         reverse(nums,0,k-1);
         reverse(nums,k,nums.length-1);
