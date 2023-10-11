82ea07f5da9c14ba295f8e89a6e7cde4edf844a8
Vinay C N <70995499+Vinay-CN@users.noreply.github.com>
2023-10-11 20:52:26 +0530

    Time: 1 ms (100%), Space: 44.7 MB (88.3%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,14 +1,27 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i = 0 , j = 0 ;
-        while(j<nums.length){
-            if(nums[j]!=0) {
-                int temp = nums[j];
-                nums[j] = nums[i] ;
-                nums[i] = temp;
-                i++;
+        // int i = 0 , j = 0 ;
+        // while(j<nums.length){
+        //     if(nums[j]!=0) { //just like 0 - 1 sorting  i will be at 0  and j will be at non-zero so we can swap;
+        //         int temp = nums[j];
+        //         nums[j] = nums[i] ;
+        //         nums[i] = temp;
+        //         i++;
+        //     }
+        //     j++;
+        // }
+
+        int lastNonZeroFoundAt = 0;
+        // If the current element is not 0, then we need to
+        // append it just in front of last non 0 element we found. 
+        for(int i = 0 ; i < nums.length ; i++){
+            if(nums[i]!=0){
+                nums[lastNonZeroFoundAt++] =nums[i];
             }
-            j++;
+        }
+
+        for(int i =lastNonZeroFoundAt ;i < nums.length ; i++ ){
+            nums[i] = 0;
         }
     }
 }
