8212d77b9ff9b3929aee7a6961b4bbd5d53d68cd
Vinay C N <70995499+Vinay-CN@users.noreply.github.com>
2023-10-12 23:05:22 +0530

    Time: 30 ms (43.07%), Space: 55 MB (67.13%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,22 +1,29 @@
 class Solution {
 
     public void conquere(int[] nums ,int s,int e, int mid){
-        int i = s ;
-        int j = mid+1 ;
+        int i = s ;   // starting index of left half of arr
+        int j = mid+1 ;  // starting index of right half of arr
         int[] temp = new int[(e-s)+1];
-        int k = 0 ;
+        int k = 0 ;  
+
+
+           //storing elements in the temporary array in a sorted manner// 
+
         while(i<=mid && j<=e){
             if(nums[i]<=nums[j]) temp[k++] = nums[i++];
             else temp[k++] = nums[j++];
         }
+      // if elements on the left half are still left //
 
         while(i<=mid){
             temp[k++] = nums[i++];
         }
-
+  // if elements on the right half are still left //
         while(j<=e){
             temp[k++] = nums[j++];
         }
+
+         // transfering all elements from temporary to arr // 
         int originalIndex = s ;
         k = 0 ;
         for(int a : temp){
