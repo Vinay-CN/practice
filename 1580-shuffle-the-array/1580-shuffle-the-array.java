2ca390a4a0dc688d0221c1175cd64a76d7eb84bb
Vinay C N <70995499+Vinay-CN@users.noreply.github.com>
2023-10-16 20:12:18 +0530

    Time: 0 ms (100%), Space: 43.4 MB (84.46%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.java
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -1,51 +1,18 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        for(int i = n ; i < nums.length ; i++){
-            nums[i] = (nums[i] << 10 ) | nums[i-n]; // left shift and to pair do or operation  i 
-        } // left shifting 10 times because 1000 is max number and 1000 has 10 bits so move 10 bits
-        int index = 0 ;
-        for(int i = n ; i < nums.length ; i++ , index+=2){
-            nums[index] = nums[i] & 1023 ;  //to get the paired number do and op with all one 
-            nums[index+1] = nums[i]>>10;  // do right shift to get original number
+        int[] temp = new int[n];
+        int max = 10000; //any number greater than 10 ^ 3 
+        for(int i =0 ; i < n ; i++){
+            temp[i] = nums[i] + nums[n+i] * max; //pairing
         }
-
+        int ind = 0 ;
+        for(int num : temp){
+            nums[ind] =num%max  ; //to get first value
+            ind++;
+            nums[ind] = num/max ; //to get second value
+            ind++;
+        }
         return nums;
+
     }
 }
-
-
-
-//pairing the array
-// class Solution {
-//     public int[] shuffle(int[] nums, int n) {
-//         int j = n;
-//         for(int i = 0 ; i < n ; i++){
-//             nums[j] = nums[i]*1001 + nums[j]; // since the constraints is 1000 and we can apply euclids algo here 
-//             j++;
-//         }
-
-//         j = n ;
-//         for(int i = 0 ; i < 2 * n ; i+=2){
-//             nums[i] = nums[j]/1001 ;
-//             nums[i+1] = nums[j]%1001;
-//             j++;
-//         }
-
-//         return nums;
-//     }
-// }
-
-
-// class Solution {
-//     public int[] shuffle(int[] nums, int n) {
-//         int ans[] = new int[2*n];
-//         int j = 0 ; 
-//         for(int i = 0 ; i < 2*n ; i = i + 2){
-//                 ans[i] = nums[j];
-//                 ans[i+1] = nums[j+n] ;
-//                 j++;
-//         }
-//         return ans;
-//     }
-// }
-
