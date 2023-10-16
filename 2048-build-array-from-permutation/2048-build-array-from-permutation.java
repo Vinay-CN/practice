06d5cf2b2e0dbeab1ebf7e7456fafb9482474352
Vinay C N <70995499+Vinay-CN@users.noreply.github.com>
2023-10-16 18:07:04 +0530

    Time: 1 ms (98.21%), Space: 43.9 MB (91.11%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.java
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.java
@@ -1,45 +1,9 @@
 class Solution {
     public int[] buildArray(int[] nums) {
-        int n = nums.length ;
-        for(int i = 0 ; i < n ; i++){
-            nums[i] = nums[i] + 1001 * (nums[nums[i]]% 1001); //euclid algorithm d = d*q + r ; 39 = 5 * 7 + 4
+        int ans[] = new int[nums.length];
+        for(int i = 0 ; i < nums.length ; i++){
+            ans[i] = nums[nums[i]];
         }
-        // since the constraints is 1000 and we can apply euclids algo here 
-
-        for(int i = 0 ; i < n ; i++){
-            nums[i] = nums[i]/1001;
-        }
-        return nums;
+        return ans;
     }
 }
-
-
-
-//recursion 
-// class Solution {
-    
-//     public int[] buildArray(int[] nums) {
-//        rec(nums , 0 );
-//        return nums; 
-//     }
-
-//     public void rec(int[] nums , int start){
-//         if(start<nums.length){
-//             int temp = nums[start];
-//             int res = nums[temp];
-//             rec(nums,start+1);
-//             nums[start] = res ;
-//         }
-//     }
-// }
-
- //brute force
-// class Solution {
-//     public int[] buildArray(int[] nums) {
-//         int[] ans = new int[nums.length];
-//         for(int i = 0 ; i < nums.length ; i++){
-//             ans[i] = nums[nums[i]];
-//         }
-//         return ans;
-//     }
-// }
