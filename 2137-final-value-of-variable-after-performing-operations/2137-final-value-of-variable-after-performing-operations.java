6674640fea08e15f6f43d0582855cc24d281e31c
Vinay C N <70995499+Vinay-CN@users.noreply.github.com>
2023-10-16 19:57:51 +0530

    Time: 0 ms (100%), Space: 41.1 MB (87.45%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
@@ -1,10 +1,21 @@
+// class Solution {
+//     public int finalValueAfterOperations(String[] operations) {
+//         int ans = 0 ;
+//         for(int i = 0 ; i < operations.length;i++)
+//         {
+//             String temp  = operations[i];
+//             if(temp.charAt(0) == '+' || temp.charAt(2) =='+') ans++;
+//             else ans--;
+//         }
+
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int finalValueAfterOperations(String[] operations) {
-        int x = 0 ;
-        for(String s : operations){
-            if(s.charAt(1) =='-') x--;
-            else x++;
-        }
+        int x = 0;
+        for(String o : operations) x += (44 - o.charAt(1)); //asci code of + is 43 and -  is 45  so 44 - 43 is 1 and 44-45 = -1
         return x;
     }
 }
