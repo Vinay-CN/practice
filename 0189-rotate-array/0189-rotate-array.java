9957d9986f3d0ad16f4b98aacfa945d4edc050a2
Vinay C N <70995499+Vinay-CN@users.noreply.github.com>
2023-10-18 18:54:02 +0530

    Time: 0 ms (100%), Space: 56 MB (37.31%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -11,7 +11,7 @@ class Solution {
     public void rotate(int[] nums, int k) {
         k = k%nums.length ; //if k == nums.length then rotated array will be same so we need to rotate it k%n times;
         reverse(nums,0,nums.length-1);
-        reverse(nums,0,k-1);
+        reverse(nums,0,k-1); //if k = 101 then we will move out of range so mod it
         reverse(nums,k,nums.length-1);
     }
 }
