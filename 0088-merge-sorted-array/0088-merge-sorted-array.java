4b89976d41c9654eae3205942d7aa059665b3d10
Vinay C N <70995499+Vinay-CN@users.noreply.github.com>
2023-10-19 11:34:55 +0530

    Time: 0 ms (100%), Space: 41.6 MB (14.65%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -6,9 +6,9 @@ class Solution {
 
     }
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if  ( n == 0 ) return ;
+        if  ( n == 0 ) return ; //if nums2 is zero then no need to do things
  
-        if(m == 0 ) {
+        if(m == 0 ) { //if first array is zero then copy all ele from nus2 to nums1
             int i = 0 ;
             while(i<n){
                 nums1[i] = nums2[i] ;
@@ -16,23 +16,24 @@ class Solution {
             } 
             return ;
         }
-        int i = m-1; 
-        int j = (m+n) - 1  ;
-        int k = n -1 ;
-        while(i>=0 && k>=0){
-            if( nums1[i] > nums2[k] ){
+        int i = m-1;   //pointer at m -1
+        int j = (m+n) - 1  ; //pointer at last
+        int k = n -1 ; //pointer at last of nums2
+        while(i>=0 && k>=0){           
+            if( nums1[i] > nums2[k] ){ //nums1 > nums2 then swap i and j 
                 swap( i, j, nums1 );
                 i--;
                 j--;
             }
             else {
-                nums1[j] = nums2[k];
+                nums1[j] = nums2[k]; //else copy elemtent from k and shift to nums1
                 k--;
                 j--;
             }
         }
-        while(j>=0 && k>=0){
-            nums1[j] = nums2[k];
+        while(j>=0 && k>=0){ //if j >= 0 && k>=0  means there are still elemnts left in nums2 copy them thats it 2 0 and 
+        //  1 example i is at 2 , j is at 0 and k is at  1 and 2 > 1 so swap it and i goes beyond zero but elemts still thenre in nums2 
+           nums1[j] = nums2[k];
             k--;
             j--;
         }
