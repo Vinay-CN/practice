0734ca7d0e90ba6153dae69d477a79bc88da4a89
Vinay C N <70995499+Vinay-CN@users.noreply.github.com>
2023-10-20 17:13:17 +0530

    Time: 2 ms (100%), Space: 86.7 MB (86.29%) - LeetHub

--- 1803-average-waiting-time/1803-average-waiting-time.java
+++ 1803-average-waiting-time/1803-average-waiting-time.java
@@ -5,8 +5,8 @@ class Solution {
         for(int[] curr : customers){
             int aT = curr[0];
             int cT = curr[1];
-            if(aT>totalTime) totalTime = aT;
-            totalTime+=cT;
+            if(aT>totalTime) totalTime = aT; // cook was free after 15 and new arrived at 20 so we take total as him
+            totalTime+=cT; // add the cooking time + total time and sub with my arrival time
            
             avg+= totalTime - aT;
         }
