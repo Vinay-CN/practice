561a1674756a6fb342809a34ace7320b40c478e0
Vinay C N <70995499+Vinay-CN@users.noreply.github.com>
2023-10-21 18:45:26 +0530

    Time: 5 ms (67.34%), Space: 56.4 MB (72.54%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.java
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.java
@@ -1,31 +1,17 @@
-
 class Solution {
     public boolean canArrange(int[] arr, int k) {
+        int[] freq = new int[k];
+        for(int num : arr){
+            int rem = num%k;
+            if(rem<0) rem+=k;
+            freq[rem]++;
+        }
+
         
-        HashMap<Integer,Integer> map = new HashMap<>();
-        
-        for(int i = 0 ; i < arr.length; i++){
-            
-            int rem = ((arr[i]%k)+k)%k;//to convert to positive
-            
-            map.put(rem,map.getOrDefault(rem,0)+1);
-            }
-        
-        
-        for(int key : map.keySet()) {
-    		
-    		
-    		if(key == 0) {
-    			
-    			if(map.get(key)%2!=0) //if zero means i will not have pair so i want to make sure i am even
-    				return false;
-    		}
-    		
-    		else if(!map.get(key).equals(map.get(k-key)))  //i will copmare my rem to key - rem : 2 5 - 3 = 2 both must be same
-    			return false;
-    	}
-        
-        return true;
-        
+        for(int i = 1 ; i < k ; i++){
+            if(freq[i]!=freq[k-i]) return false;
+        }
+        return (freq[0]%2==0) ;
+    
     }
 }
