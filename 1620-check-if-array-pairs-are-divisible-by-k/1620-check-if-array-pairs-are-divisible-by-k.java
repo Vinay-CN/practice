5d59f9840f813c769268276601aeeb3095053ce7
Vinay C N <70995499+Vinay-CN@users.noreply.github.com>
2023-10-21 17:57:34 +0530

    Time: 35 ms (34.68%), Space: 58.1 MB (40.75%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.java
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.java
@@ -6,7 +6,7 @@ class Solution {
         
         for(int i = 0 ; i < arr.length; i++){
             
-            int rem = ((arr[i]%k)+k)%k;
+            int rem = ((arr[i]%k)+k)%k;//to convert to positive
             
             map.put(rem,map.getOrDefault(rem,0)+1);
             }
@@ -17,11 +17,11 @@ class Solution {
     		
     		if(key == 0) {
     			
-    			if(map.get(key)%2!=0)
+    			if(map.get(key)%2!=0) //if zero means i will not have pair so i want to make sure i am even
     				return false;
     		}
     		
-    		else if(!map.get(key).equals(map.get(k-key)))
+    		else if(!map.get(key).equals(map.get(k-key)))  //i will copmare my rem to key - rem : 2 5 - 3 = 2 both must be same
     			return false;
     	}
         
