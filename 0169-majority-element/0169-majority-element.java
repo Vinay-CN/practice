80830e58921359323afdb1f57bf898237ce95872
Vinay C N <70995499+Vinay-CN@users.noreply.github.com>
2023-10-23 23:20:23 +0530

    Time: 2 ms (68.29%), Space: 48.2 MB (64.68%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,17 +1,21 @@
+//bruteforece is 2 nested loops
+//better approach is hasmap frequency 
+//optimal is this
+//initially count will be zero then the current element is majEle 
 class Solution {
     public int majorityElement(int[] nums) {
         int count = 0 ;
         int majorityElement  = Integer.MAX_VALUE;
         for(int num : nums){
-            if(count == 0) {
-                majorityElement = num ;
-                count++;
+            if(count == 0) { //if count 0 curr ele is majELe
+                majorityElement = num ; 
+                count++; //increase the current ele count
             }
             else if(num==majorityElement){
-                count++;
+                count++; //if num is same as majEle then increase count
             }
             else{
-                count--;
+                count--; //if not decrease count at last we will get highest  freq number
             }
         }
         return majorityElement ;
