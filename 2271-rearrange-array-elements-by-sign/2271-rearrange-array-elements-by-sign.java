a8733f6a526984105784e017293041b7b4da5d6b
Vinay C N <70995499+Vinay-CN@users.noreply.github.com>
2023-10-25 15:38:58 +0530

    Time: 3 ms (100%), Space: 84.7 MB (13.07%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,26 +1,54 @@
 class Solution {
+    //using 1 pass and O(N) space
+    //we know that every odd index will have neg value so if we find neg we will put in ans and increase index by 2
+    //if even means we will put in even index
     public int[] rearrangeArray(int[] nums) {
-        int n = nums.length ;
+        int n = nums.length;
+        int[] ans = new int[n];
+        int posIndex = 0, negIndex = 1;
+        for (int i = 0; i < n; i++) {
 
-       int[] pos = new int[n/2];
-       int[] neg = new int[n/2]; 
-        int posI = 0;
-        int negI = 0 ;
-       for(int i = 0 ; i < n ; i++){
-           if(nums[i]<0) {
-               neg[negI++] = nums[i];
-           }
-           else{
-               pos[posI++] = nums[i];
-           }
-       }
-        posI = 0;
-        negI = 0 ;
-       for(int i = 0 ; i < n/2 ; i++){
-           nums[2*i] = pos[posI++];
-           nums[2*i+1] = neg[negI++] ; 
-       }
+            // Fill negative elements in odd indices and inc by 2.
+            if (nums[i] < 0) {
+                ans[negIndex] =  nums[i];
+                negIndex += 2;
+            }
 
-       return nums;
+            // Fill positive elements in even indices and inc by 2.
+            else {
+                ans[posIndex] =  nums[i];
+                posIndex += 2;
+            }
+        }
+
+        return ans;
     }
 }
+
+//using two pass and O(N) sPAce
+// class Solution {
+//     public int[] rearrangeArray(int[] nums) {
+//         int n = nums.length ;
+
+//        int[] pos = new int[n/2];
+//        int[] neg = new int[n/2]; 
+//         int posI = 0;
+//         int negI = 0 ;
+//        for(int i = 0 ; i < n ; i++){
+//            if(nums[i]<0) {
+//                neg[negI++] = nums[i];
+//            }
+//            else{
+//                pos[posI++] = nums[i];
+//            }
+//        }
+//         posI = 0;
+//         negI = 0 ;
+//        for(int i = 0 ; i < n/2 ; i++){
+//            nums[2*i] = pos[posI++];
+//            nums[2*i+1] = neg[negI++] ; 
+//        }
+
+//        return nums;
+//     }
+// }
