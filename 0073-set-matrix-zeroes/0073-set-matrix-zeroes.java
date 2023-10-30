f65ffecfa57ffc848cee40abcceecc7e44deb1d0
Vinay C N <70995499+Vinay-CN@users.noreply.github.com>
2023-10-30 13:58:26 +0530

    Time: 1 ms (89.63%), Space: 44.4 MB (77.7%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,4 +1,24 @@
-//brute force is making non zeros as   -1 
+class Solution {
+    public void setZeroes(int[][] matrix) {
+    int col0 = 1, rows = matrix.length, cols = matrix[0].length;
+
+    for (int i = 0; i < rows; i++) {
+        if (matrix[i][0] == 0) col0 = 0;
+        for (int j = 1; j < cols; j++)
+            if (matrix[i][j] == 0)
+                matrix[i][0] = matrix[0][j] = 0;
+    }
+
+    for (int i = rows - 1; i >= 0; i--) {
+        for (int j = cols - 1; j >= 1; j--){
+                 if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                matrix[i][j] = 0;
+        }
+           
+        if (col0 == 0) matrix[i][0] = 0;
+    }
+}   }
+  /*  //brute force is making non zeros as   -1 
 //better is using one extra row array and col array
 //better approach
 class Solution {
@@ -84,7 +104,7 @@ class Solution {
 
 
 //brute force approach without using extra space 
-/*class Solution {
+class Solution {
 public void  setZero(int i , int j , int[][] matrix){
         for(int k=0;k<matrix[0].length;k++){
             if(matrix[i][k]!=0) {
